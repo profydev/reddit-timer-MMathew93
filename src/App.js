a5b1455267b5e205e8c5cca1cf2d0c684ce63d9e
Mathew McKinney <58349502+MMathew93@users.noreply.github.com>
2021-04-01 15:30:08 -0700

    Following changes per todo requirements: (#1)
    * Installed fonts, added reset for browser styles, added global styling, added router and set up two pages
    
    * changed app title and meta description, updated favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import LandingPage from './Views/LandingPage';
+import SearchPage from './Views/SearchPage';
+import './Styling/App.css';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <LandingPage />
+        </Route>
+        <Route exact path="/search">
+          <SearchPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Views/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>
+        THIS IS THE LANDING/HOME PAGE!
+      </h1>
+    </div>
+  );
+}
+
+export default LandingPage;
