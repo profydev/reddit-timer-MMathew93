7ece3c97b130a524bfcfab63ce40443e74468f4b
lyndexer <mckinney.m93@gmail.com>
2021-03-31 17:43:56 -0700

    Added components folder, added header file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/Header';
 import LandingPage from './Views/LandingPage';
 import SearchPage from './Views/SearchPage';
 import './Styling/App.css';
@@ -7,6 +8,7 @@ import './Styling/App.css';
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/">
           <LandingPage />
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      Header PlaceHolder
+    </header>
+  );
+}
+
+export default Header;
