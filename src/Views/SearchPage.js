5a4e0c4a49da25a56527bf6fc35771a01baa6afb
lyndexer <mckinney.m93@gmail.com>
2021-03-31 17:22:43 -0700

    Installed fonts, added reset for browser styles, added global styling, added router and set up two pages

---
+++ src/Views/SearchPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function SearchPage() {
+  return (
+    <div>
+      <h1>
+        THIS IS THE SEARCH PAGE!
+      </h1>
+    </div>
+  );
+}
+
+export default SearchPage;
