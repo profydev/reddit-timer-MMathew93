64330b3b9cc56f5984f8151da60e2d57cdb84090
lyndexer <mckinney.m93@gmail.com>
2021-04-03 14:45:45 -0700

    imported header logo, imported fonts, set links for navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../images_and_logos/logo.svg';
+import '../Styling/Header.css';
 
 function Header() {
   return (
-    <header>
-      Header PlaceHolder
-    </header>
+    <>
+      <header>
+        <Link to="/">
+          <img className="Logo" src={Logo} alt="Reddit Timer Logo" />
+        </Link>
+        <nav>
+          <Link className="NavLinks" to="/search/javascript">
+            Search
+          </Link>
+          <Link className="NavLinks" to="/#how-it-works">
+            How it works
+          </Link>
+          <Link className="NavLinks" to="/#about">
+            About
+          </Link>
+        </nav>
+      </header>
+    </>
   );
 }
 
